d346e1a40384d12a62d465a98a627dfd65a22342
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-05-27 17:16:48 -0400

    [tx-robot] updated from transifex

--- l10n/ur_PK.php
+++ l10n/ur_PK.php
@@ -1,3 +1,6 @@
-<?php $TRANSLATIONS = array(
-"Error" => "ایرر"
+<?php
+$TRANSLATIONS = array(
+"Download" => "ڈاؤن لوڈ،",
+"Delete" => "حذف کریں"
 );
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
--- l10n/zh_CN.php
+++ l10n/zh_CN.php
@@ -13,6 +13,8 @@ $TRANSLATIONS = array(
 "Moving files..." => "正在移动文件...",
 "All done. Click to the link below to start database upgrade." => "已全部完成。点击下方链接开始进行数据库更新。",
 "Update failed" => "更新失败",
+"The update was unsuccessful. Please check logs at admin page and report this issue to the <a href=\"https://github.com/owncloud/apps/issues\" target=\"_blank\">ownCloud community</a>." => "更新不成功。请在管理页面检查日志并且将此问题汇报给<a href=\"https://github.com/owncloud/apps/issues\" target=\"_blank\">ownCloud 社区</a>。",
+"Proceed" => "已处理",
 "Update Center" => "更新中心",
 "Backup Management" => "备份管理",
 "Update" => "更新",
