099473f3b0d3c37ebabf79abf39f5f6d906e93cb
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-07-17 00:16:29 -0400

    [tx-robot] updated from transifex

--- l10n/bg_BG.php
+++ l10n/bg_BG.php
@@ -1,8 +1,29 @@
 <?php
 $TRANSLATIONS = array(
-"Here is your backup: " => "Тук е Вашия архив:",
+"Checking your installation..." => "Проверка на инсталацията ви...",
+"Upgrade is not possible. Make sure that your webserver has write access to the following files and directories:" => "Обновяването не е възможно. Проверете дали уеб сървърът има права за запис в следните папки и файлове:",
+"Version not found" => "Версията не е открита",
+"Upgrade is not possible. Your webserver has not enough permissions to remove the following directory:" => "Обновяването не е възможно. Уеб сървърът няма права да премахне следната папка:",
+"Update permissions on this directory and its content or remove it manually first." => "Първо обновете правата на тази папка и съдържанието й или я изтрийте.",
+"Downloading package..." => "Изтегляне на пакет...",
+"Unable to fetch package" => "Грешка при изтеглянето на пакет",
+"Creating backup..." => "Създаване на архив...",
+"Here is your backup: " => "Тук е Вашият архив:",
+"Failed to create backup" => "Грешка при създаването на архив",
+"Moving files..." => "Преместване на файлове...",
+"All done. Click to the link below to start database upgrade." => "Готово, натиснете препратката по-долу за обновяване на базата от данни.",
+"Update failed" => "Грешка при обновяването",
+"The update was unsuccessful. Please check logs at admin page and report this issue to the <a href=\"https://github.com/owncloud/apps/issues\" target=\"_blank\">ownCloud community</a>." => "Обновяването е неуспешно. Проверете записите в дневника на страницата за управление и докладвайте проблема в <a href=\"https://github.com/owncloud/apps/issues\" target=\"_blank\">общността на ownCloud</a>.",
+"Proceed" => "Продължаване",
+"Updater" => "Обновяване",
+"Update Center" => "Център за обновления",
+"Backup Management" => "Управление на архивите",
 "Update" => "Обновяване",
+"Backup directory" => "Папка за архиви",
+"Backup" => "Архивиране",
+"Done on" => "направено на",
 "Download" => "Изтегляне",
-"Delete" => "Изтриване"
+"Delete" => "Изтриване",
+"No backups found" => "Не са открити архиви"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
