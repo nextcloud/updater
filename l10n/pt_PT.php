f9483f5742b805a4c7175a19296b20792b5b5d33
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-08-20 00:16:12 -0400

    [tx-robot] updated from transifex

--- l10n/pt_PT.php
+++ l10n/pt_PT.php
@@ -4,6 +4,7 @@ $TRANSLATIONS = array(
 "Upgrade is not possible. Your webserver has not enough permissions to remove the following directory:" => "A atualização não é possivel. O seu servidor web nao tem permissões para remover a seguinte diretoria:",
 "Update permissions on this directory and its content or remove it manually first." => "Atualize as permissões nesta diretoria e no seu conteudo ou remova manualmente.",
 "The update was unsuccessful. Please check logs at admin page and report this issue to the <a href=\"https://github.com/owncloud/apps/issues\" target=\"_blank\">ownCloud community</a>." => "A atualização falhou. Por favor verifique os registos na página de administração e relate este assunto à <a href=\"https://github.com/owncloud/apps/issues\" target=\"_blank\">comunidade ownCloud</a>.",
+"Retry" => "Tentar novamente",
 "Here is your backup: " => "Aqui está a sua cópia de segurança:",
 "Proceed" => "Continuar",
 "All done. Click to the link below to start database upgrade." => "Tudo feito. Clique no atalho abaixo para iniciar a atualização da base de dados.",
