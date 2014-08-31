37e7b745ab9bea37ddb4528f1cbbc5e7b5f198a4
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-08-31 00:16:13 -0400

    [tx-robot] updated from transifex

--- l10n/cs_CZ.php
+++ l10n/cs_CZ.php
@@ -5,7 +5,7 @@ $TRANSLATIONS = array(
 "Update permissions on this directory and its content or remove it manually first." => "Upravte oprávnění u tohoto adresáře a jeho obsahu nebo jej nejdřív ručně smažte.",
 "Update failed." => "Aktualizace se nezdařila.",
 "The update was unsuccessful. Please check logs at admin page and report this issue to the <a href=\"https://github.com/owncloud/apps/issues\" target=\"_blank\">ownCloud community</a>." => "Aktualizace byla neúspěšná. Zkontrolujte, prosím, logy v administračním rozhraní a oznamte tuto událost  <a href=\"https://github.com/owncloud/apps/issues\" target=\"_blank\">komunitě ownCloud</a>.",
-"Server error. Please check webserver log file for details" => "Chyba na serveru. Pro více informací zkontrolujte prosím serverové logy.",
+"Server error. Please check webserver log file for details" => "Chyba na serveru. Pro více informací zkontrolujte prosím serverové logy",
 "Retry" => "Zkusit znovu",
 "Here is your backup: " => "Zde je vaše záloha:",
 "Proceed" => "Provést",
@@ -22,6 +22,6 @@ $TRANSLATIONS = array(
 "Download" => "Stáhnout",
 "Delete" => "Smazat",
 "No backups found" => "Žádné zálohy nebyly nalezeny",
-"Up to date. Checked on %s" => "Je v poslední verzi. Ověřeno na %s"
+"Up to date. Checked on %s" => "Je v poslední verzi. Ověřeno %s"
 );
 $PLURAL_FORMS = "nplurals=3; plural=(n==1) ? 0 : (n>=2 && n<=4) ? 1 : 2;";
