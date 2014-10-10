241d24f4fec136290c405b378f04a74d11ecf62b
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-10-10 00:16:15 -0400

    [tx-robot] updated from transifex

--- l10n/es.php
+++ l10n/es.php
@@ -1,29 +1,28 @@
 <?php
 $TRANSLATIONS = array(
 "Version not found" => "Versión no encontrada",
-"Upgrade is not possible. Your webserver has not enough permissions to remove the following directory:" => "No se puede actualizar. Su servidor web no tiene los permisos necesarios para eliminar la siguiente carpeta:",
 "Update permissions on this directory and its content or remove it manually first." => "Actualice los permisos de esta carpeta y sus contenidos o elimínela manualmente.",
 "Update failed." => "No se pudo actualizar.",
 "The update was unsuccessful. Please check logs at admin page and report this issue to the <a href=\"https://github.com/owncloud/apps/issues\" target=\"_blank\">ownCloud community</a>." => "La actualización ha fallado. Por favor, revisa la razón del fallo en el log de la página de admin y reporta este problema a la <a href=\"https://github.com/owncloud/apps/issues\" target=\"_blank\">Comunidad de ownCloud</ a>.",
-"Server error. Please check webserver log file for details" => "Error en el servidor. Revise el archivo de bitácora del servidor web para más detalles",
 "Retry" => "Volver a intentar",
 "Here is your backup: " => "Aquí está tu copia de seguridad:",
 "Proceed" => "Proceder",
 "All done. Click to the link below to start database upgrade." => "Todo listo. Haga clic en el siguiente enlace para iniciar la actualización de la base de datos",
 "Please ask your server administrator to enable PHP zip extension." => "Pídale al administrador del servidor que active la extensión zip de PHP.",
 "Not possible to update %s to %s. Downgrading or skipping major releases is not supported." => "No se puede actualizar %s a %s. Tampoco es posible retroceder a una versión anterior o saltarse una versión importante.",
-"Make sure that your webserver has read access to the following files and directories:" => "Verifique que el servidor web tenga acceso a los siguientes archivos y directorios:",
-"Make sure that your webserver has write access to the following files and directories:" => "Verifique que el servidor web tenga permiso de escritura para los siguientes archivos y directorios:",
+"Make sure that your web server has read access to the following files and directories:" => "Asegúrese de que su servidor tenga acceso de lectura a los siguientes archivos y carpetas:",
+"Make sure that your web server has write access to the following files and directories:" => "Asegúrese de que su servidor tenga acceso de escritura a los siguientes archivos y carpetas:",
 "Updater" => "Actualizador",
-"Update Center" => "Centro de actualizaciones",
-"Backup Management" => "Administración de respaldos",
+"Open Update Center" => "Abrir centro de actualizaciones",
+"Updates" => "Actualizaciones",
+"A new version is available: %s" => "Hay una nueva versión disponible: %s",
+"Up to date. Checked on %s" => "Tiene la versión actual. Revisado en %s",
 "Update" => "Actualizar",
 "Backup directory" => "Directorio de respaldos",
+"No backups found" => "No se encontraron copias de seguridad",
 "Backup" => "Copia de seguridad",
 "Done on" => "Hecho en",
 "Download" => "Descargar",
-"Delete" => "Eliminar",
-"No backups found" => "No se encontraron copias de seguridad",
-"Up to date. Checked on %s" => "Tiene la versión actual. Revisado en %s"
+"Delete" => "Eliminar"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
