e14f06686aeb0755931d38bc853687731a968132
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-12-10 00:16:12 -0500

    [tx-robot] updated from transifex

--- l10n/ja.php
+++ l10n/ja.php
@@ -7,6 +7,7 @@ $TRANSLATIONS = array(
 "The update was unsuccessful. Please check logs at admin page and report this issue to the <a href=\"https://github.com/owncloud/apps/issues\" target=\"_blank\">ownCloud community</a>." => "アップデートに失敗しました。管理画面でログを確認して、この問題を <a href=\"https://github.com/owncloud/core/issues\" target=\"_blank\">ownCloudコミュニティ</a> に報告してください。",
 "Server error. Please check web server log file for details" => "サーバーエラー。詳細に関してはWebサーバーのログを確認してください",
 "Retry" => "リトライ",
+"Here is your backup:" => "こちらはあなたのバックアップです:",
 "Proceed" => "続行",
 "All done. Click to the link below to start database upgrade." => "すべて完了しました。データベースのアップグレードを開始するには、以下のリンクをクリックしてください。",
 "Please ask your server administrator to enable PHP zip extension." => "サーバーの管理者に PHP zip 機能を有効にするよう依頼してください。",
