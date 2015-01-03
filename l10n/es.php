aefc470174f8003f2fc7ff926aa87af66fc1cce6
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-01-03 00:16:10 -0500

    [tx-robot] updated from transifex

--- l10n/es.php
+++ l10n/es.php
@@ -4,6 +4,7 @@ $TRANSLATIONS = array(
 "Upgrade is not possible. Your web server does not have permission to remove the following directory:" => "No se puede actualizar. Su servidor web no tiene permiso para eliminar el siguiente directorio:",
 "Update permissions on this directory and its content or remove it manually first." => "Actualice los permisos de esta carpeta y sus contenidos o elimínela manualmente.",
 "Update failed." => "Fallo la Actualización",
+"The update was unsuccessful. Please check logs at admin page and report this issue to the <a href=\"https://github.com/owncloud/updater/issues/new\" target=\"_blank\">ownCloud community</a>." => "La actualización ha fallado. Por favor, revisa la razón del fallo en el log de la página de admin y reporta este problema a la <a href=\"https://github.com/owncloud/updater/issues/new\" target=\"_blank\">ownCloud community</a>.",
 "Server error. Please check web server log file for details" => "Error en el servidor. Por favor vea los registros del servidor por mas detalles,",
 "Retry" => "Reintentar",
 "Here is your backup:" => "Aquí está su copia de seguridad:",
