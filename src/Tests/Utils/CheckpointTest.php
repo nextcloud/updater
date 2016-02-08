2d2c27d68cb68cf99cdf96d4ce138859df70591f
Lukas Reschke <lukas@owncloud.com>
2016-02-08 23:21:54 +0100

    Only do a scandir if the folder exists

--- src/Tests/Utils/CheckpointTest.php
+++ src/Tests/Utils/CheckpointTest.php
@@ -7,7 +7,7 @@ use Owncloud\Updater\Utils\FilesystemHelper;
 use Owncloud\Updater\Utils\Locator;
 
 class CheckpointTest extends \PHPUnit_Framework_TestCase {
-	public function testGetAll(){
+	public function testGetAll() {
 		$checkpointList = ['a', 'b', 'c'];
 		$fsHelper = $this->getMockBuilder('Owncloud\Updater\Utils\FilesystemHelper')
 				->disableOriginalConstructor()
@@ -15,6 +15,9 @@ class CheckpointTest extends \PHPUnit_Framework_TestCase {
 		;
 		$fsHelper->method('scandir')
 				->willReturn($checkpointList)
+		;
+		$fsHelper->method('isDir')
+			->willReturn(true)
 		;
 		$locator = $this->getMockBuilder('Owncloud\Updater\Utils\Locator')
 				->disableOriginalConstructor()
@@ -25,7 +28,26 @@ class CheckpointTest extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals($checkpointList, $actual);
 	}
 
-
+	public function testGetAllWithNotExistingFolder() {
+		$checkpointList = ['a', 'b', 'c'];
+		$fsHelper = $this->getMockBuilder('Owncloud\Updater\Utils\FilesystemHelper')
+			->disableOriginalConstructor()
+			->getMock()
+		;
+		$fsHelper->method('scandir')
+			->willReturn($checkpointList)
+		;
+		$fsHelper->method('isDir')
+			->willReturn(false)
+		;
+		$locator = $this->getMockBuilder('Owncloud\Updater\Utils\Locator')
+			->disableOriginalConstructor()
+			->getMock()
+		;
+		$checkpointMock = $this->getCheckpointInstance($locator, $fsHelper);
+		$actual = $checkpointMock->getAll();
+		$this->assertEquals([], $actual);
+	}
 
 	protected function getCheckpointInstance(Locator $locator, FilesystemHelper $fsHelper){
 		return new Checkpoint($locator, $fsHelper);
