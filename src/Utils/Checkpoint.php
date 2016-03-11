62b1ce2be8d9b68387666278aec8549481f2cca2
Victor Dubiniuk <victor.dubiniuk@gmail.com>
2016-03-11 19:16:03 +0300

    Removing checkpoints

--- src/Utils/Checkpoint.php
+++ src/Utils/Checkpoint.php
@@ -44,15 +44,21 @@ class Checkpoint {
 	/**
 	 *
 	 * @param Locator $locator
+	 * @param FilesystemHelper $fsHelper
 	 */
 	public function __construct(Locator $locator, FilesystemHelper $fsHelper){
 		$this->locator = $locator;
 		$this->fsHelper = $fsHelper;
 	}
 
+	/**
+	 * Creates a checkpoint
+	 * @return string
+	 * @throws Exception if base checkpoint directory is not writable
+	 */
 	public function create(){
-		$checkpointName = $this->getCheckpointName();
-		$checkpointPath = $this->locator->getCheckpointDir() . '/' . $checkpointName;
+		$checkpointId = $this->createCheckpointId();
+		$checkpointPath = $this->getCheckpointPath($checkpointId);
 		try{
 			if (!$this->fsHelper->isWritable($this->locator->getCheckpointDir())){
 				throw new \Exception($this->locator->getCheckpointDir() . ' is not writable.');
@@ -91,29 +97,42 @@ class Checkpoint {
 			$this->fsHelper->removeIfExists($checkpointPath);
 			throw $e;
 		}
-		return $checkpointName;
+		return $checkpointId;
 	}
 
+	/**
+	 * Restore a checkpoint by id
+	 * @param string $checkpointId id of checkpoint
+	 * @return array
+	 * @throws UnexpectedValueException if there is no checkpoint with this id
+	 */
 	public function restore($checkpointId){
+		$this->assertCheckpointExists($checkpointId);
 		$checkpointDir = $this->locator->getCheckpointDir() . '/' . $checkpointId;
-		if (!$this->fsHelper->fileExists($checkpointDir)){
-			$message = sprintf('Checkpoint %s does not exist.', $checkpointId);
-			throw new \UnexpectedValueException($message);
-		}
 		$ocRoot = $this->locator->getOwncloudRootPath();
 		$this->fsHelper->copyr($checkpointDir . '/' . self::CORE_DIR, $ocRoot, false);
 		$this->fsHelper->copyr($checkpointDir . '/' . self::APP_DIR, $ocRoot . '/' . self::APP_DIR, false);
 	}
 
+	/**
+	 * Remove a checkpoint by id
+	 * @param string $checkpointId id of checkpoint
+	 * @return array
+	 * @throws UnexpectedValueException if there is no checkpoint with this id
+	 */
+	public function remove($checkpointId){
+		$this->assertCheckpointExists($checkpointId);
+		$checkpointPath = $this->getCheckpointPath($checkpointId);
+		$this->fsHelper->removeIfExists($checkpointPath);
+	}
+
+	/**
+	 * Return all checkpoints as an array of items [ 'title', 'date' ]
+	 * @return array
+	 */
 	public function getAll(){
 		$checkpoints = [];
-		$checkpointDir = $this->locator->getCheckpointDir();
-		$content = $this->fsHelper->isDir($checkpointDir) ? $this->fsHelper->scandir($checkpointDir) : [];
-
-		foreach ($content as $dir){
-			if (in_array($dir, ['.', '..'])){
-				continue;
-			}
+		foreach ($this->getAllCheckpointIds() as $dir){
 			$checkpoints[] = [
 				'title' => $dir,
 				'date' => date(
@@ -127,9 +146,59 @@ class Checkpoint {
 		return $checkpoints;
 	}
 
-	protected function getCheckpointName(){
+	/**
+	 * Check if there is a checkpoint with a given id
+	 * @param string $checkpointId id of checkpoint
+	 * @return bool
+	 */
+	public function checkpointExists($checkpointId){
+		return in_array($checkpointId, $this->getAllCheckpointIds());
+	}
+
+	/**
+	 * Return array of all checkpoint ids
+	 * @return array
+	 */
+	protected function getAllCheckpointIds(){
+		$checkpointDir = $this->locator->getCheckpointDir();
+		$content = $this->fsHelper->isDir($checkpointDir) ? $this->fsHelper->scandir($checkpointDir) : [];
+		$checkpoints = array_filter(
+ 			$content,
+ 			function($dir){
+ 				$checkpointPath = $this->getCheckpointPath($dir);
+ 				return !in_array($dir, ['.', '..']) && $this->fsHelper->isDir($checkpointPath);
+ 			}
+ 		);
+ 		return $checkpoints;
+	}
+
+	/**
+	 * Create an unique checkpoint id
+	 * @return string
+	 */
+	protected function createCheckpointId(){
 		$versionString = implode('.', $this->locator->getInstalledVersion());
 		return uniqid($versionString . '-');
 	}
 
+	/**
+	 * Get an absolute path to the checkpoint directory by checkpoint Id
+	 * @param string $checkpointId id of checkpoint
+	 * @return string
+	 */
+	protected function getCheckpointPath($checkpointId){
+		return $this->locator->getCheckpointDir() . '/' . $checkpointId;
+	}
+
+	/**
+	 * Produce an error on non-existing checkpoints
+	 * @param string $checkpointId id of checkpoint
+	 * @throws UnexpectedValueException if there is no checkpoint with this id
+	 */
+	private function assertCheckpointExists($checkpointId){
+		if (!$this->checkpointExists($checkpointId) || $checkpointId === ''){
+			$message = sprintf('Checkpoint %s does not exist.', $checkpointId);
+			throw new \UnexpectedValueException($message);
+		}
+	}
 }
