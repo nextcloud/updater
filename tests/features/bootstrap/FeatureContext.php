3948ff06114ef7e5385b53463aabe85154a850b8
Morris Jobke <hey@morrisjobke.de>
2016-10-27 11:08:48 +0200

    add first tests
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

---
+++ tests/features/bootstrap/FeatureContext.php
@@ -0,0 +1,136 @@
+<?php
+
+use Behat\Behat\Context\Context;
+use Behat\Behat\Tester\Exception\PendingException;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext implements Context
+{
+    protected $buildDir;
+    protected $serverDir;
+    protected $tmpDownloadDir;
+    protected $downloadURL = 'https://download.nextcloud.com/server/releases/';
+
+    public function __construct()
+    {
+        $baseDir = __DIR__ . '/../../data/';
+        $this->serverDir = $baseDir . 'server/';
+        $this->tmpDownloadDir = $baseDir . 'downloads/';
+        $this->buildDir = $baseDir . '../build/';
+        if(!file_exists($baseDir) && !mkdir($baseDir)) {
+            throw new RuntimeException('Creating tmp download dir failed');
+        }
+        if(!file_exists($this->serverDir) && !mkdir($this->serverDir)) {
+            throw new RuntimeException('Creating server dir failed');
+        }
+        if(!file_exists($this->tmpDownloadDir) && !mkdir($this->tmpDownloadDir)) {
+            throw new RuntimeException('Creating tmp download dir failed');
+        }
+    }
+
+    /**
+     * @Given the current installed version is ([0-9.]+)
+     */
+    public function theCurrentInstalledVersionIs($version)
+    {
+        // recursive deletion of server folder
+        if(file_exists($this->serverDir)) {
+            $iterator = new RecursiveIteratorIterator(
+                new RecursiveDirectoryIterator($this->serverDir, RecursiveDirectoryIterator::SKIP_DOTS),
+                RecursiveIteratorIterator::CHILD_FIRST
+            );
+
+            foreach ($iterator as $fileInfo) {
+                $action = $fileInfo->isDir() ? 'rmdir' : 'unlink';
+                $action($fileInfo->getRealPath());
+            }
+            $state = rmdir($this->serverDir);
+            if($state === false) {
+                throw new \Exception('Could not rmdir ' . $this->serverDir);
+            }
+        }
+
+        $filename = 'nextcloud-' . $version . '.zip';
+
+        if (!file_exists($this->tmpDownloadDir . $filename)) {
+            $fp = fopen($this->tmpDownloadDir . $filename, 'w+');
+            $ch = curl_init($this->downloadURL . $filename);
+            curl_setopt($ch, CURLOPT_FILE, $fp);
+            if(curl_exec($ch) === false) {
+                throw new \Exception('Curl error: ' . curl_error($ch));
+            }
+            $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            if($httpCode !== 200) {
+                throw new \Exception('Download failed - HTTP code: ' . $httpCode);
+            }
+            curl_close($ch);
+            fclose($fp);
+        }
+
+        $zip = new ZipArchive;
+        $zipState = $zip->open($this->tmpDownloadDir . $filename);
+        if ($zipState === true) {
+            $zip->extractTo($this->serverDir);
+            $zip->close();
+        } else {
+            throw new \Exception('Cant handle ZIP file. Error code is: '.$zipState);
+        }
+
+        chdir($this->serverDir . 'nextcloud');
+        shell_exec('chmod +x occ');
+        exec('./occ maintenance:install --admin-user=admin --admin-pass=admin', $output, $returnCode);
+
+        if ($returnCode !== 0) {
+            throw new Exception('Install failed' . PHP_EOL . join(PHP_EOL, $output));
+        }
+    }
+
+    /**
+     * @Given there is no update available
+     */
+    public function thereIsNoUpdateAvailable()
+    {
+        #throw new PendingException();
+    }
+
+    /**
+     * @When the CLI updater is run
+     */
+    public function theCliUpdaterIsRun()
+    {
+        copy($this->buildDir . 'updater', $this->serverDir . 'nextcloud/updater/updater');
+        chdir($this->serverDir . 'nextcloud/updater');
+        exec('./updater', $output, $returnCode);
+
+        if ($returnCode !== 0) {
+            throw new Exception('updater failed' . PHP_EOL . join(PHP_EOL, $output));
+        }
+    }
+
+    /**
+     * @Given there is an update to version ([0-9.]+) available
+     */
+    public function thereIsAnUpdateToVersionAvailable($version)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then the installed version should be ([0-9.]+)
+     */
+    public function theInstalledVersionShouldBe2($version)
+    {
+        /** @var $OC_Version */
+        require $this->serverDir . 'nextcloud/version.php';
+
+        $installedVersion = join('.', $OC_Version);
+        // Hack for version number mapping
+        $installedVersion = str_replace(['9.1', '9.2'], ['10.0', '11.0'], $installedVersion);
+
+        if (strpos($installedVersion, $version) !== 0) {
+            throw new Exception('Version mismatch - Installed: ' . $installedVersion . ' Wanted: ' . $version);
+        }
+    }
+}
---
+++ tests/features/cli.feature
@@ -0,0 +1,14 @@
+Feature: CLI updater
+
+  Scenario: No update is available - 10.0.0
+    Given the current installed version is 10.0.0
+    And there is no update available
+    When the CLI updater is run
+    Then the installed version should be 10.0.0
+
+  Scenario: Update is available - 10.0.0 to 1
+    Given the current installed version is 10.0.0
+    And there is an update to version 10.0.1 available
+    When the CLI updater is run
+    Then the installed version should be 10.0.1
+
