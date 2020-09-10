0032e2188b4176c2e350fe17fc5ad51cff84fa92
Morris Jobke <hey@morrisjobke.de>
2020-09-10 11:57:17 +0200

    Updater.phar updated
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInit994b8d870ddb923ebc3ff0ceaaaa96a6
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
