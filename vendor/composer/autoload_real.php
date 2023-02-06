53a2fce8a1e93e0b7bed56e299383510619ac2f9
Côme Chilliet <come.chilliet@nextcloud.com>
2023-02-06 14:57:25 +0100

    Make updater.phar with uptodate composer
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -35,15 +35,15 @@ class ComposerAutoloaderInitd7f5ddc48e4715174279359c16c76340
         $loader->register(true);
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInitd7f5ddc48e4715174279359c16c76340::$files;
-        $requireFile = static function ($fileIdentifier, $file) {
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
             if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
                 $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
 
                 require $file;
             }
-        };
+        }, null, null);
         foreach ($filesToLoad as $fileIdentifier => $file) {
-            ($requireFile)($fileIdentifier, $file);
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
