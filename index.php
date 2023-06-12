ef3a0cd6967ed32dd0c47478bf3e60383770bdf2
Côme Chilliet <come.chilliet@nextcloud.com>
2023-06-12 09:34:07 +0200

    Make sure all CI steps can run on PHP 8.0
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

--- index.php
+++ index.php
@@ -650,7 +650,7 @@ class Updater {
 		}
 
 		$response = $this->getUpdateServerResponse();
-		if (!isset($response['signature'])) {
+		if (empty($response['signature'])) {
 			throw new \Exception('No signature specified for defined update');
 		}
 
