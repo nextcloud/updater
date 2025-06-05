e8d3fcfa32340b4082ad1407e504e89aa7091e1f
Benjamin Gaussorgues <benjamin.gaussorgues@nextcloud.com>
2025-06-05 17:46:33 +0200

    chore(download): allows Updater to follow redirects
    Signed-off-by: Benjamin Gaussorgues <benjamin.gaussorgues@nextcloud.com>

--- lib/Updater.php
+++ lib/Updater.php
@@ -504,6 +504,8 @@ class Updater {
 			CURLOPT_RETURNTRANSFER => 1,
 			CURLOPT_URL => $updateURL,
 			CURLOPT_USERAGENT => 'Nextcloud Updater',
+			CURLOPT_FOLLOWLOCATION => 1,
+			CURLOPT_MAXREDIRS => 2,
 		]);
 
 		if ($this->getConfigOption('proxy') !== null) {
