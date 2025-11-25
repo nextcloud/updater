0046df602f4b42b13821734009b6a5edea47764f
Benjamin Gaussorgues <benjamin.gaussorgues@nextcloud.com>
2025-11-25 11:39:49 +0100

    fix(download): download fail with only one URL
    Signed-off-by: Benjamin Gaussorgues <benjamin.gaussorgues@nextcloud.com>

--- lib/Updater.php
+++ lib/Updater.php
@@ -572,6 +572,9 @@ class Updater {
 			if (!$this->isAbleToDecompress($format)) {
 				continue;
 			}
+
+			// If only one download URL exists, $urls is a string
+			$urls = (array)$urls;
 			foreach ($urls as $url) {
 				if (!is_string($url)) {
 					continue;
