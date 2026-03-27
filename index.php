bb54499156b2e77ebfed108bbdd98aee846ab042
Ferdinand Thiessen <opensource@fthiessen.de>
2026-03-27 20:14:18 +0100

    fix: verify integrity with URL override but allow to disable if needed
    Before:
    As soon as an URL override was used no integrity verification was done.
    
    Now:
    Always do integrity verification (to allow custom mirrors or our signed
    files!). But to also allow third party files where we cannot verify the
    integrity we allow passing `--no-verify` commandline flag.
    
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- index.php
+++ index.php
@@ -758,16 +758,11 @@ class Updater {
 	 *
 	 * @throws \Exception
 	 */
-	public function verifyIntegrity(?string $urlOverride = null): void {
+	public function verifyIntegrity(): void {
 		$this->silentLog('[info] verifyIntegrity()');
 
 		if ($this->getCurrentReleaseChannel() === 'daily') {
 			$this->silentLog('[info] current channel is "daily" which is not signed. Skipping verification.');
-			return;
-		}
-
-		if ($urlOverride) {
-			$this->silentLog('[info] custom download url provided, cannot verify signature');
 			return;
 		}
 
